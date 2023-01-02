fc98da589acd2383279c17834fa166f40d257722
andreolg <andreolg@cs.washington.edu>
2023-01-01 19:16:22 -0800

    Make more changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 (function() {
 
 let slideIndex = 1;
+let timeoutID = 0;
 
 
 window.onload = function() {
@@ -22,7 +23,41 @@ window.onload = function() {
    });
     document.getElementById('dot-4').addEventListener('click', () => {
      currentSlide(4);
-   })
+   });
+    document.getElementById('dot-5').addEventListener('click', () => {
+     currentSlide(5);
+   });
+
+    document.getElementById('instagram').addEventListener('mouseover', () => {
+     text.innerText = 'instagram';
+     text.classList.remove('hidden');
+     clearTimeout(timeoutId);
+   });
+    document.getElementById('youtube').addEventListener('mouseover', () => {
+     text.innerText = 'youtube';
+     text.classList.remove('hidden');
+     clearTimeout(timeoutId);
+   });
+    document.getElementById('website').addEventListener('mouseover', () => {
+     text.innerText = 'website';
+     text.classList.remove('hidden');
+     clearTimeout(timeoutId);
+   });
+    document.getElementById('instagram').addEventListener('mouseleave', () => {
+     timeoutID = setTimeout(() => {
+       text.classList.add('hidden');
+     }, 3000);
+   });
+    document.getElementById('youtube').addEventListener('mouseleave', () => {
+     timeoutID = setTimeout(() => {
+       text.classList.add('hidden');
+     }, 3000);
+   });
+    document.getElementById('website').addEventListener('mouseleave', () => {
+     timeoutID = setTimeout(() => {
+       text.classList.add('hidden');
+     }, 3000);
+   });
 };
 
 // Next/previous controls
